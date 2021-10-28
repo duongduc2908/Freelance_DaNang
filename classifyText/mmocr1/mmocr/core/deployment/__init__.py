131090fbd59c9d269fe19882d289b95a9ded51cd
PhiDCH <59147911+PhiDCH@users.noreply.github.com>
2021-10-28 17:38:19 +0700

    Delete classifyText/mmocr1 directory

--- classifyText/mmocr1/mmocr/core/deployment/__init__.py
+++
@@ -1,7 +0,0 @@
-from .deploy_utils import (ONNXRuntimeDetector, ONNXRuntimeRecognizer,
-                           TensorRTDetector, TensorRTRecognizer)
-
-__all__ = [
-    'ONNXRuntimeRecognizer', 'ONNXRuntimeDetector', 'TensorRTDetector',
-    'TensorRTRecognizer'
-]
