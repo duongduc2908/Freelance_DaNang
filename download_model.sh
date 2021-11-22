bc8102729b98679ad5e018eaf3769a5e76416f11
duongduc2908 <37935298+duongduc2908@users.noreply.github.com>
2021-11-22 14:29:01 +0700

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -5,7 +5,7 @@ cd ../../
 if ! [ -f "checkpoints.zip" ]
 then
     # gdown --id 1269bExpsdHUI8g3wROapdOxboaKoyPNr
-    gdown --id 1BURNNtys19yl2XmBR9DE6xqTjyr3uuaU
+    gdown --id 1EATn7u_ZZCSMmQFbu3EXrS6v-l2eVere
     unzip checkpoints.zip
     rm -f checkpoints.zip
 fi
